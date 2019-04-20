da0f3200c189b90894cb2840cff8fd358fa9d49b
vauxhall <zero.griffin@gmail.com>
2019-04-20 18:00:41 +0200

    Create test subtree

---
+++ Tests/TestSubtree.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import Git
+
+class TestSubtree: XCTestCase {
+    private var url: URL!
+    
+    override func setUp() {
+        url = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("test")
+        try! FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
+    }
+    
+    override func tearDown() {
+        try? FileManager.default.removeItem(at: url)
+    }
+}
