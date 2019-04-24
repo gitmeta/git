9e06f05c362f5e6ae13f2e33c5ae8688c7158021
vauxhall <zero.griffin@gmail.com>
2019-04-24 15:58:35 +0200

    Update commit view

--- Mac/Tools.swift
+++ Mac/Tools.swift
@@ -12,25 +12,25 @@ class Tools: NSView {
         let commit = Button(target: self, action: #selector(self.commit))
         commit.image = NSImage(named: "commit")
         commit.imageScaling = .scaleNone
-        commit.width.constant = 52
-        commit.height.constant = 30
+        commit.width.constant = 45
+        commit.height.constant = 45
         addSubview(commit)
         
         height = heightAnchor.constraint(equalToConstant: 0)
         height.isActive = true
         
-        commit.rightAnchor.constraint(equalTo: rightAnchor, constant: -10).isActive = true
-        commit.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
+        commit.centerXAnchor.constraint(equalTo: centerXAnchor, constant: -10).isActive = true
+        commit.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20).isActive = true
     }
     
     required init?(coder: NSCoder) { return nil }
     
     @objc private func commit() {
-        App.shared.repository?.user.name = "john"
-        App.shared.repository?.user.email = "john@mail.com"
+//        App.shared.repository?.user.name = "john"
+//        App.shared.repository?.user.email = "john@mail.com"
         App.shared.repository?.commit(
             (App.shared.list.documentView!.subviews as! [Item]).filter({ $0.stage.state == .on }).map { $0.url },
-            message:"hello world", error: {
+            message:"Git commit", error: {
                 App.shared.alert.show($0.localizedDescription)
         })
     }
