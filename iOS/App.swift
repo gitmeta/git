35fa2daf6249b7b031efcaccbe65246f3b7fa749
vauxhall <zero.griffin@gmail.com>
2019-05-06 10:34:11 +0200

    Add file sharing

--- iOS/App.swift
+++ iOS/App.swift
@@ -11,6 +11,8 @@ import UIKit
         rootViewController = UIViewController()
         if #available(iOS 11.0, *) { margin = rootViewController!.view.safeAreaInsets }
         
+        try! "hello world\n".write(to: FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("file.txt"), atomically: true, encoding: .utf8)
+        
         return true
     }
     
