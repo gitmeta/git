7bf01b026a798065f7c6018bfe3d8d0bc233bff4
vauxhall <zero.griffin@gmail.com>
2019-05-15 08:05:05 +0200

    Validate packed object with adler

--- Git/Pack.swift
+++ Git/Pack.swift
@@ -84,14 +84,18 @@ class Pack {
             default: break
             }
             
-            var index = parse.index + 1
+            let content = Hub.press.unpack(expected, data: parse.data.subdata(in: parse.index ..< parse.data.count))
+            parse.discard(content.0)
+            /*var index = parse.index + 1
             var content = Data()
 //            print(category)
-            while content.count <= expected {
+            while content.count < expected {
                 index += 1
                 content = Hub.press.decompress(parse.data.subdata(in: parse.index ..< index))
-            }
-            items.append((category, content))
+            }*/
+            print(category)
+            items.append((category, content.1))
+            /*
 //            print(String(decoding: content, as: UTF8.self))
             parse.discard(index - parse.index)
             
@@ -105,11 +109,15 @@ class Pack {
             
             if parse.index == 4396 {
                 parse.discard(-1)
+                let adler = Hub.press.adler32(content)
+                
                 debugPrint("\(try parse.byte()) \(try parse.byte()) \(try parse.byte()) \(try parse.byte())")
+                
+                debugPrint("\(adler[0]) \(adler[1]) \(adler[2]) \(adler[3])")
                 fatalError()
             }
 //            parse.discard(4)
-            debugPrint("\(try parse.byte()) \(try parse.byte()) \(try parse.byte()) \(try parse.byte())")
+            debugPrint("\(try parse.byte()) \(try parse.byte()) \(try parse.byte()) \(try parse.byte())")*/
         }
         guard parse.data.count - parse.index == 20 else { throw Failure.Pack.invalidPack }
     }
--- Git/Press.swift
+++ Git/Press.swift
@@ -29,6 +29,34 @@ class Press {
         return data
     }
     
+    func unpack(_ size: Int, data: Data) -> (Int, Data) {
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
+        let scratch = UnsafeMutablePointer<UInt8>.allocate(capacity: self.size)
+        var result = Data()
+        var index = 2
+        while result.count < size {
+            index += 1
+            result = data.subdata(in: 2 ..< index).withUnsafeBytes {
+                let read = compression_decode_buffer(buffer, size, $0.baseAddress!.bindMemory(to: UInt8.self, capacity: 1),
+                                                     index - 2, scratch, COMPRESSION_ZLIB)
+                return Data(bytes: buffer, count: read)
+            } as Data
+        }
+        buffer.deallocate()
+        scratch.deallocate()
+        let adler = adler32(result)
+        
+        while
+            data[index] != adler[0] &&
+            data[index + 1] != adler[1] &&
+            data[index + 2] != adler[2] &&
+            data[index + 3] != adler[3] {
+            index += 1
+        }
+        index += 4
+        return (index, result)
+    }
+    
     private func adler32(_ data: Data) -> Data {
         var s1 = UInt32(1 & 0xffff)
         var s2 = UInt32((1 >> 16) & 0xffff)
