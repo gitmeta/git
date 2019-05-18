599e2e5cc23f0655705c68a4a20446c460408a51
vauxhall <zero.griffin@gmail.com>
2019-05-18 17:07:17 +0200

    Implement reset on iOS

---
+++ iOS/Reset.swift
@@ -0,0 +1,79 @@
+import Git
+import UIKit
+
+class Reset: Sheet {
+    @discardableResult override init() {
+        super.init()
+        let image = UIImageView(image: #imageLiteral(resourceName: "error.pdf"))
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .center
+        image.clipsToBounds = true
+        addSubview(image)
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.attributedText = {
+            $0.append(NSAttributedString(string: .local("Reset.title"), attributes: [.font: UIFont.systemFont(ofSize: 16, weight: .bold)]))
+            $0.append(NSAttributedString(string: .local("Reset.subtitle"), attributes: [.font: UIFont.systemFont(ofSize: 14, weight: .light)]))
+            return $0
+        } (NSMutableAttributedString())
+        label.textColor = .white
+        label.textAlignment = .center
+        addSubview(label)
+        
+        let confirm = UIButton()
+        confirm.translatesAutoresizingMaskIntoConstraints = false
+        confirm.addTarget(self, action: #selector(self.confirm), for: .touchUpInside)
+        confirm.setTitle(.local("Reset.confirm"), for: [])
+        confirm.setTitleColor(.black, for: .normal)
+        confirm.setTitleColor(UIColor(white: 0, alpha: 0.2), for: .highlighted)
+        confirm.titleLabel!.font = .systemFont(ofSize: 14, weight: .medium)
+        confirm.backgroundColor = .halo
+        confirm.layer.cornerRadius = 6
+        addSubview(confirm)
+        
+        let cancel = UIButton()
+        cancel.translatesAutoresizingMaskIntoConstraints = false
+        cancel.addTarget(self, action: #selector(close), for: .touchUpInside)
+        cancel.setTitle(.local("Reset.cancel"), for: [])
+        cancel.setTitleColor(.white, for: .normal)
+        cancel.setTitleColor(UIColor(white: 1, alpha: 0.2), for: .highlighted)
+        cancel.titleLabel!.font = .systemFont(ofSize: 14, weight: .medium)
+        addSubview(cancel)
+        
+        image.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        image.bottomAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        label.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 20).isActive = true
+        label.widthAnchor.constraint(lessThanOrEqualToConstant: 200).isActive = true
+        
+        confirm.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        confirm.heightAnchor.constraint(equalToConstant: 36).isActive = true
+        confirm.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        confirm.bottomAnchor.constraint(equalTo: cancel.topAnchor, constant: -20).isActive = true
+        
+        cancel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        cancel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        cancel.heightAnchor.constraint(equalToConstant: 36).isActive = true
+        
+        if #available(iOS 11.0, *) {
+            cancel.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -40).isActive = true
+        } else {
+            cancel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -40).isActive = true
+        }
+    }
+    
+    required init?(coder: NSCoder) { return nil }
+    
+    @objc private func confirm() {
+        close()
+        App.repository?.reset({
+            App.view.alert.error($0.localizedDescription)
+        }) {
+            App.view.alert.update(.local("Reset.success"))
+            App.view.refresh()
+        }
+    }
+}
--- iOS/Tools.swift
+++ iOS/Tools.swift
@@ -36,6 +36,15 @@ class Tools: UIView {
         credentials.imageView!.contentMode = .center
         addSubview(credentials)
         
+        let reset = UIButton()
+        reset.translatesAutoresizingMaskIntoConstraints = false
+        reset.addTarget(self, action: #selector(self.reset), for: .touchUpInside)
+        reset.setImage(#imageLiteral(resourceName: "resetOff.pdf"), for: .normal)
+        reset.setImage(#imageLiteral(resourceName: "resetOn.pdf"), for: .highlighted)
+        reset.imageView!.clipsToBounds = true
+        reset.imageView!.contentMode = .center
+        addSubview(reset)
+        
         heightAnchor.constraint(equalToConstant: 100).isActive = true
 
         commit.widthAnchor.constraint(equalToConstant: 65).isActive = true
@@ -52,11 +61,17 @@ class Tools: UIView {
         credentials.heightAnchor.constraint(equalToConstant: 50).isActive = true
         credentials.rightAnchor.constraint(equalTo: log.leftAnchor, constant: -20).isActive = true
         credentials.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -10).isActive = true
+        
+        reset.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        reset.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        reset.leftAnchor.constraint(equalTo: commit.rightAnchor, constant: 20).isActive = true
+        reset.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -10).isActive = true
     }
     
     required init?(coder: NSCoder) { return nil }
     @objc private func log() { Log() }
     @objc private func preferences() { Credentials() }
+    @objc private func reset() { Reset() }
     @objc func commit() {
         if Hub.session.name.isEmpty || Hub.session.email.isEmpty {
             preferences()
