b74831b48a38cfe95faa3b38ea0ebea7137764df
vauxhall <zero.griffin@gmail.com>
2019-05-24 14:26:29 +0200

    Clean up press

--- Git/Press.swift
+++ Git/Press.swift
@@ -30,59 +30,37 @@ class Press {
     }
     
     func unpack(_ size: Int, data: Data) throws -> (Int, Data) {
-        var stream = UnsafeMutablePointer<compression_stream>.allocate(capacity: 1).pointee
-        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
-        let scratch = UnsafeMutablePointer<UInt8>.allocate(capacity: max(size, 50_000_000))
-        
-        var index = max(alead(size, data: data) - max(size / 50, 3), 0)
-        
-        
-        /*var index = max(data.withUnsafeBytes {
-            Data(bytesNoCopy: buffer, count: compression_decode_buffer( buffer, size, $0.baseAddress!.bindMemory(
-                to: UInt8.self, capacity: 1).advanced(by: 2), data.count - 2, scratch, COMPRESSION_ZLIB), deallocator: .none)
-        }.withUnsafeBytes {
-            assert($0.count == size)
-            return compression_encode_buffer(buffer, size, $0.baseAddress!.bindMemory(to: UInt8.self, capacity: 1), size, scratch, COMPRESSION_ZLIB)
-        } - 3, 0)*/
+        var index = max(compress(decompress(data)).count - max(size / 35, 9), 0)
         let result = try data.withUnsafeBytes {
+            var stream = UnsafeMutablePointer<compression_stream>.allocate(capacity: 1).pointee
+            let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
             var status = compression_stream_init(&stream, COMPRESSION_STREAM_DECODE, COMPRESSION_ZLIB)
             var advance = 2
             var read = index + 1
             var result = Data()
-            var times = 0
-            print("read: \(read), advance: \(advance), index: \(index)")
             repeat {
-                
-                
                 index += 1
                 stream.dst_ptr = buffer
                 stream.dst_size = size
                 stream.src_size = read
                 stream.src_ptr = $0.baseAddress!.bindMemory(to: UInt8.self, capacity: 1).advanced(by: advance)
                 status = compression_stream_process(&stream, 0)
-                result += Data(bytesNoCopy: buffer, count: size - stream.dst_size, deallocator: .none)
+                result += Data(bytes: buffer, count: size - stream.dst_size)
                 read = 1
                 advance = 2 + index
-                times += 1
             } while status == COMPRESSION_STATUS_OK
-//            print("stop \(data[index]) \(data[index + 1]) \(data[index + 2])")
-            print("times \(times)")
-            guard status == COMPRESSION_STATUS_END else {
-                throw Failure.Pack.read
-            }
+            buffer.deallocate()
+            compression_stream_destroy(&stream)
+            guard status == COMPRESSION_STATUS_END else { throw Failure.Pack.read }
+            guard result.count == size else { throw Failure.Pack.size }
             return result
         } as Data
-        guard result.count == size else {
-            throw Failure.Pack.size
-        }
         
         let adler = adler32(result)
         var found = false
         var drift = 0
         repeat {
-            if drift > 7 {
-                throw Failure.Pack.adler
-            }
+            if drift > 3 { throw Failure.Pack.adler }
             if adler[0] == data[index + drift],
                 adler[1] == data[index + drift + 1],
                 adler[2] == data[index + drift + 2],
@@ -93,33 +71,7 @@ class Press {
         } while !found
         
         index += 6
-        buffer.deallocate()
-        scratch.deallocate()
-        compression_stream_destroy(&stream)
         return (index, result)
-    }
-    
-    func alead(_ size: Int, data: Data) -> Int {
-        let decoded = decompress(data)
-        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
-        var stream = UnsafeMutablePointer<compression_stream>.allocate(capacity: 1).pointee
-        let size = decoded.withUnsafeBytes {
-            var status = compression_stream_init(&stream, COMPRESSION_STREAM_ENCODE, COMPRESSION_ZLIB)
-            stream.dst_ptr = buffer
-            stream.dst_size = size
-            stream.src_size = decoded.count
-            stream.src_ptr = $0.baseAddress!.bindMemory(to: UInt8.self, capacity: 1)
-            status = compression_stream_process(&stream, Int32(COMPRESSION_STREAM_FINALIZE.rawValue))
-            return size - stream.dst_size
-        } as Int
-        buffer.deallocate()
-        compression_stream_destroy(&stream)
-        return size
-    }
-    
-    private func decompress(_ data: Data, size: Int, buffer: UnsafeMutablePointer<UInt8>, scratch: UnsafeMutablePointer<UInt8>) -> Data {
-        return data.withUnsafeBytes { Data(bytes: buffer, count: compression_decode_buffer(
-            buffer, size, $0.baseAddress!.bindMemory(to: UInt8.self, capacity: 1), data.count, scratch, COMPRESSION_ZLIB)) }
     }
     
     private func adler32(_ data: Data) -> Data {
